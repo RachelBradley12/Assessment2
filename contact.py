894489827ed0f4eba36806bf7154fdbc2e0af9a9
c1012894 <r.bradley12@newcastle.ac.uk>
2021-11-10 11:49:10 +0000

    Added Contact class Added addContact function Added Main Menu Added searchExisting function

--- contact.py
+++ contact.py
@@ -20,15 +20,7 @@ def show_main_menu():
           "Enter 4 To Quit\n**********************")
     choice = input("Enter your choice: ")
     if choice == "1":
-        contactsfile = open("contactList.txt", "w+")
-        file_contents = contactsfile.read()
-        if len(file_contents) == 0:
-            print("Phone Book is empty")
-        else:
-            print(file_contents)
-        #contactsfile.close
-        end = input("Press Enter to continue ...")
-        show_main_menu()
+        displayContacts()
     elif choice == "2":
         addContact()
     elif choice == "3":
@@ -36,8 +28,20 @@ def show_main_menu():
 
 
 
-def addContact():
+def displayContacts():
+    contactsfile = open("contactList.txt", "r+")
+    file_contents = contactsfile.read()
+    if len(file_contents) == 0:
+        print("Phone Book is empty")
+    else:
+        print(file_contents)
+    input("Press Enter to return to Main Menu ...")
+    show_main_menu()
 
+
+
+def addContact():
+    # This function writes the user inputs for each contact detail and write it into the text file
     readContacts = open("contactList.txt", "r+")
     list_of_contacts = []
     for line in readContacts:
@@ -59,6 +63,7 @@ def addContact():
 
 
 def addAnother():
+    # This function asks the user if they would like to add another contact
     another = input("\nWould you like to add another contact? (Y or N) ")
     if another == 'Y':
         addContact()
@@ -71,27 +76,114 @@ def addAnother():
 def search_existing():
     # This function searches for an existing contact and displays the result
     choice = int(input("Enter search criteria\n\n\
-1. Name\n2. Address\n3. Number\n4. Birthday\n\n\
+Name: 1\nAddress: 2\nPhone Number: 3\nBirthday: 4\n\n\
 Please enter: "))
     # We're doing so just to ensure that the user experiences a customized search result
 
     if choice == 1:
-        search_name = str(input("Please enter the name of the contact you wish to search: "))
-        rem_name = str(choice[1:])
-        first_char = choice[0]
-        search_name = first_char.upper() + rem_name
-        contactsfile = open("contactList.txt", 'r+')
-        file_contents = contactsfile.readlines()
+        # This will execute for searches based on contact name
+        searched_name = str(input("Please enter the name of the contact you wish to search: "))
+        contactsfile = open("contactList.txt", "r+")
 
-        found = False
-        for line in file_contents:
-            if search_name in line:
-                print("Your Required Contact Record is:" + search_name)
-                print(line)
-                found = True
+        # setting flag to 0
+        flag = 0
+
+        # Loop through the file line by line
+        for line in contactsfile:
+
+            # checking string is present in line or not
+            if searched_name in line:
+                flag = 1
                 break
-        if found == False:
-            print("There's no contact Record in Phone Book with name = " + search_name)
+
+        # checking condition for string found or not
+        if flag == 0:
+            print("Contact Name, ", searched_name, ", Not Found in Contacts Book")
+        else:
+            print("Contact Name, ", searched_name, ", Found!")
+            print(line)
+        # closing text file
+        contactsfile.close()
+
+
+    elif choice == 2:
+        # This will execute for searches based on contact address
+        searched_address = str(input("Please enter the address of the contact you wish to search: "))
+        contactsfile = open("contactList.txt", "r+")
+
+        # setting flag to 0
+        flag = 0
+
+        # Loop through the file line by line
+        for line in contactsfile:
+
+            # checking string is present in line or not
+            if searched_address in line:
+                flag = 1
+                break
+
+        # checking condition for string found or not
+        if flag == 0:
+            print("Contact Address, ", searched_address, ", Not Found in Contacts Book")
+        else:
+            print("Contact Address, ", searched_address, ", Found!")
+            print(line)
+        # closing text file
+        contactsfile.close()
+
+
+    elif choice == 3:
+        # This will execute for searches based on contact phone number
+        searched_number = str(input("Please enter the phone number of the contact you wish to search: "))
+        contactsfile = open("contactList.txt", "r+")
+
+        # setting flag to 0
+        flag = 0
+
+        # Loop through the file line by line
+        for line in contactsfile:
+
+            # checking string is present in line or not
+            if searched_number in line:
+                flag = 1
+                break
+
+        # checking condition for string found or not
+        if flag == 0:
+            print("Contact Phone Number, ", searched_number, ", Not Found in Contacts Book")
+        else:
+            print("Contact Phone Number, ", searched_number, ", Found!")
+            print(line)
+        # closing text file
+        contactsfile.close()
+
+    elif choice == 4:
+        # This will execute for searches based on contact phone number
+        searched_birthday = str(input("Please enter the birthday of the contact you wish to search: "))
+        contactsfile = open("contactList.txt", "r+")
+
+        # setting flag to 0
+        flag = 0
+
+        # Loop through the file line by line
+        for line in contactsfile:
+
+            # checking string is present in line or not
+            if searched_birthday in line:
+                flag = 1
+                break
+
+        # checking condition for string found or not
+        if flag == 0:
+            print("Contact with Birthday, ", searched_birthday, ", Not Found in Contacts Book")
+        else:
+            print("Contact with Birthday, ", searched_birthday, ", Found!")
+            print(line)
+        # closing text file
+        contactsfile.close()
+
+    input("Press Enter to return to Main Menu ...")
+    show_main_menu()
 
 
 
