f286ac12db84c071350dbb399db3f4909c633b74
c1012894 <r.bradley12@newcastle.ac.uk>
2021-11-16 13:21:02 +0000

    Added Contact class Added add_contact function Added Main Menu Addes display function Added search_existing function Added change_contact function Can now chnage the name of a contact Added some validation to the add_contact function

--- contact.py
+++ contact.py
@@ -27,7 +27,7 @@ def show_main_menu():
     elif choice == "3":
         search_existing()
     elif choice == "4":
-        change_contact()
+        change_details()
     else:
         print("Error!\nPlease enter either numbers 1, 2, 3, or 4")
         show_main_menu()
@@ -212,111 +212,63 @@ Please enter: "))
     show_main_menu()
 
 
-def change_contact():
-    # This function searches for changing details of an existing contact
-    contact = str(input("Enter the Name of the Contact whose details you wish to change: "))
-    contactsfile = open("contactList.txt", "r+")
+def change_details():
+    with open('contactList.txt') as f:
+        contact_data = f.read().splitlines()
+        searched_name = str(input("Enter the Name of the Contact whose details you wish to change: "))
 
-    # setting flag to 0
-    flag = 0
+        iterate_list = []
+        for contact in contact_data:
+            contact = contact.split(", ")
+            iterate_list.append(contact)
 
-    # Loop through the file line by line
-    for line in contactsfile:
-        # checking string is present in line or not
-        if contact in line:
-            flag = 1
-            break
+        for contact in iterate_list:
+            if searched_name in contact:
+                detail_to_change = input("\nWhat details  do you wish to change of this contact?\n\nName: 1\nAddress: 2\nPhone Number: 3\nBirthday: 4\n\nPlease enter: ")
 
-    # checking condition for string found or not
-    if flag == 0:
-        print("Contact Name, ", contact, ", Not Found in Contacts Book")
-    else:
-        # HERE IS WHERE WE CHANGE THE CONTACT DETAILS******
-        print("Contact Name, ", contact, ", Found!")
-        change = int(input("\nWhat details  do you wish to change of this contact?\n\nName: 1\nAddress: 2\nPhone Number: 3\nBirthday: 4\n\nPlease enter: "))
+                print(contact)
 
-        if change == 1:
-            new_name = str(input("Updated Contact Name: "))
-            text = open("contactList.txt", "r")
-            text = ''.join([i for i in text])
-            text = text.replace(contact, new_name)
-
-            x = open ("contactList.txt", "w")
-            x.writelines(text)
-            x.close()
-
-            input("Name changed! Press Enter to return to the Main Menu")
+                if detail_to_change == "1":
+                    contact[0] = input("\nChange " + contact[0] + " to what?:  ")
+                elif detail_to_change == "2":
+                    contact[1] = input("\nChange " + contact[1] + " to what?:  ")
+                elif detail_to_change == "3":
+                    contact[2] = input("\nChange " + contact[2] + " to what?:  ")
+                elif detail_to_change == "4":
+                    contact[3] = input("\nChange " + contact[3] + " to what?:  ")
 
 
+        for x in iterate_list:
+            print(*x)
 
 
+        with open("contactList.txt", "w+") as f:
+            for list in iterate_list:
+                    f.write(str(", ".join(list)) + '\n')
 
 
+        contactsfile = open("contactList.txt", "r+")
 
+        # setting flag to 0
+        flag = 0
 
+        # Loop through the file line by line
+        for line in contactsfile:
 
+            # checking exact string is present in line or not
+            if re.search(r"\b{}\b".format(contact), line):
+                flag = 1
+                break
 
-        if change == 2:
-            with open('contactList.txt') as f:
-                lines = f.read().splitlines()
-                print(lines)
-                contact_editing = lines.index(contact)
-                print(contact_editing)
+        # checking condition for string found or not
+        if flag == 0:
+            print("\nError! Contact details could not be edited")
+        else:
+            print("\nContact details edited successfully!")
+        # closing text file
+        contactsfile.close()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # new_address = str(input("Updated Contact Address: "))
-            # text = open("contactList.txt", "r")
-            # text = ''.join([i for i in text])
-            # text = text.replace(contact, new_address)
-            #
-            # x = open("contactList.txt", "w")
-            # x.writelines(text)
-            # x.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # r = csv.reader(open('contactList.csv'))  # Here your csv file
-            # lines = list(r)
-            #
-            # new_name = str(input("Updated Contact Name: "))
-            #
-            # #Modifying the values:
-            # lines[2][1] = '30'
-            #
-            #
-            # # Now we only have to write it back to a file
-            # writer = csv.writer(open('contactList.csv', 'w'))
-            # writer.writerows(lines)
-
+    input("Press Enter to return to Main Menu ...")
+    show_main_menu()
 
 show_main_menu()
